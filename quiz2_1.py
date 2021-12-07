06302c90e77e26e00c29e5a7773e72589ba94e81
p22jiacono <p22jiacono@fairfieldprep.org>
2021-12-07 13:12:00 -0500

    add quiz2_1

---
+++ quiz2_1.py
@@ -0,0 +1,13 @@
+#Author: JTI 12/7/21
+
+day = input("Enter Day of the cycle: ")
+day = day.lower()
+
+if day == "a":
+    print("You have class today because it is" + day + "day!")
+elif day == "c":
+    print("You have class today because it is" + day + "day!")
+elif day == "e":
+    print("You have class today because it is" + day + "day!")
+else:
+    print("You do not have class today!")
